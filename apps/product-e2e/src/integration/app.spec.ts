02b0e029a7acf241f21faf66b68be75bb3405329
Fazal123asd <41240844+Fazal123asd@users.noreply.github.com>
2022-07-25 16:37:02 +0530

    added cypress tests

--- apps/product-e2e/src/integration/app.spec.ts
+++ apps/product-e2e/src/integration/app.spec.ts
@@ -3,11 +3,19 @@ import { getGreeting } from '../support/app.po';
 describe('product', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should display search label', () => {
+    cy.get('label').should('contain', 'Search by Name');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome product');
+  it('should display search input', () => {
+    cy.get('input').should('be.visible');
+  });
+
+  it('should display search button', () => {
+    cy.get('button').should('contain', 'Search');
+  });
+
+  it('should display charecter cards', () => {
+    cy.get('[data-testid="1"]').should('be.visible');
   });
 });
--- apps/product/pages/index.tsx
+++ apps/product/pages/index.tsx
@@ -1,47 +1,48 @@
 import { useState } from 'react';
-import SearchBox from '../components/search-box/SearchBox'
+import SearchBox from '../components/search-box/SearchBox';
 import CardList from '../components/card-list/CardList';
 
 export function Index(props) {
-const {data} = props
-const {results} = data
-const [searchField, setSearchField] = useState('');
-const [filteredCard, setFilterCard] = useState(results);
+  const { data } = props;
+  const { results } = data;
+  const [searchField, setSearchField] = useState('');
+  const [filteredCard, setFilterCard] = useState(results);
 
-const onSearch = () =>{
-  const newFilteredMonsters = results.filter((result) => {
-    return result.name.toLocaleLowerCase().includes(searchField);
-  });
+  const onSearch = () => {
+    const newFilteredMonsters = results.filter((result) => {
+      return result.name.toLocaleLowerCase().includes(searchField);
+    });
 
-  setFilterCard(newFilteredMonsters);
-  
-}
-const onSearchChange = (event) => {
-  const searchFieldString = event.target.value.toLocaleLowerCase();
-  setSearchField(searchFieldString);
-};
+    setFilterCard(newFilteredMonsters);
+  };
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
   return (
-    <div >
-      <SearchBox 
-       onChangeHandler={onSearchChange}
-       onSearchHandler={onSearch}
-        placeholder='search'/>
-        <CardList humans={filteredCard}/>
-    </div>
+    <>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        onSearchHandler={onSearch}
+        placeholder="search"
+      />
+      <CardList humans={filteredCard} />
+    </>
   );
 }
 
 export default Index;
 
 export async function getServerSideProps() {
- const res = await fetch(`https://rickandmortyapi.com/api/character/`)
-  const data = await res.json()
+  const res = await fetch(`https://rickandmortyapi.com/api/character/`);
+  const data = await res.json();
   if (!data) {
     return <p>Loading...</p>;
   }
   // Pass data to the page via props
-  return { props: {
-    data: data
-  }
-  }
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
